e4296535598206bcc6fea159071f722f9710cadd
shift02 <shift02ss@gmail.com>
2014-10-19 17:30:14 +0900

    追加 : 鉱石野菜
    ・鉱石野菜を追加

--- src/main/java/shift/sextiarysector/SSItems.java
+++ src/main/java/shift/sextiarysector/SSItems.java
@@ -32,6 +32,8 @@ public class SSItems {
 	//素材
 	public static Item dustWaterLily;
 
+	public static Item stoneDust;
+
 	public static Item blueStoneDust;
 	public static Item yellowStoneDust;
 
@@ -51,9 +53,14 @@ public class SSItems {
 	public static Item turnip;
 	public static Item cucumber;
 
+	public static Item ironTurnip;
+
 	public static Item onion;
 	public static Item tomato;
 	public static Item corn;
+
+	public static Item goldenCorn;
+
 
 	public static Item eggplant;
 	public static Item sweetPotato;
@@ -87,6 +94,9 @@ public class SSItems {
 		dustWaterLily = new Item().setUnlocalizedName("ss.dust_waterlily").setTextureName("sextiarysector:dust/waterlily_dust").setCreativeTab(SextiarySectorAPI.TabSSCore);
 		GameRegistry.registerItem(dustWaterLily, "DustWaterLily");
 
+		stoneDust = new Item().setUnlocalizedName("ss.stone_dust").setTextureName("sextiarysector:dust/stone_dust").setCreativeTab(SextiarySectorAPI.TabSSCore);
+		GameRegistry.registerItem(stoneDust, "StoneDust");
+
 		blueStoneDust = new Item().setUnlocalizedName("ss.dust_blue_stone").setTextureName("sextiarysector:dust/bluestone_dust").setCreativeTab(SextiarySectorAPI.TabSSCore);
 		GameRegistry.registerItem(blueStoneDust, "DustBlueStone");
 
@@ -117,6 +127,9 @@ public class SSItems {
 		cucumber = new ItemCrop(1, 1, 3, 4, 0, 2, false).setUnlocalizedName("ss.cucumber").setTextureName("sextiarysector:crop/cucumber");
 		GameRegistry.registerItem(cucumber, "Cucumber");
 
+		ironTurnip = new ItemCrop(0, 1, 0, 0, 0, 0, false).setUnlocalizedName("ss.iron_turnip").setTextureName("sextiarysector:crop/iron_turnip");
+		GameRegistry.registerItem(ironTurnip, "IronTurnip");
+
 
 		onion = new ItemCrop(2, 1, 1, 0, 0, 0, false).setUnlocalizedName("ss.onion").setTextureName("sextiarysector:crop/onion");
 		GameRegistry.registerItem(onion, "Onion");
@@ -126,6 +139,9 @@ public class SSItems {
 
 		corn = new ItemCrop(0, 1, 1, 6, 4, 2, false).setUnlocalizedName("ss.corn").setTextureName("sextiarysector:crop/corn");
 		GameRegistry.registerItem(corn, "corn");
+
+		goldenCorn = new ItemCrop(0, 2, 0, 0, 0, 0, false).setUnlocalizedName("ss.golden_corn").setTextureName("sextiarysector:crop/golden_corn");
+		GameRegistry.registerItem(goldenCorn, "GoldCorn");
 
 
 		eggplant = new ItemCrop(1, 1, 4, 2, 0, 0, false).setUnlocalizedName("ss.eggplant").setTextureName("sextiarysector:crop/eggplant");
