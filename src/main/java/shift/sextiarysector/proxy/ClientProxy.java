7c7346e9beaf627fa8f806c582baaeea2e2cbbfd
shift02 <shift02ss@gmail.com>
2014-11-04 23:27:26 +0900

    追加 : 液体クリスタル
    ・クラフト用の液体クリスタルを追加

--- src/main/java/shift/sextiarysector/proxy/ClientProxy.java
+++ src/main/java/shift/sextiarysector/proxy/ClientProxy.java
@@ -7,6 +7,7 @@ import net.minecraftforge.client.MinecraftForgeClient;
 import shift.sextiarysector.renderer.block.RendererBlockBottle;
 import shift.sextiarysector.renderer.block.RendererChest;
 import shift.sextiarysector.renderer.block.RendererFarmland;
+import shift.sextiarysector.renderer.block.RendererFluidCrafter;
 import shift.sextiarysector.renderer.block.RendererGearShaft;
 import shift.sextiarysector.renderer.block.RendererHole;
 import shift.sextiarysector.renderer.block.RendererMonitor;
@@ -15,6 +16,7 @@ import shift.sextiarysector.renderer.block.RendererSmallWindmill;
 import shift.sextiarysector.renderer.block.RendererWindmill;
 import shift.sextiarysector.renderer.item.RenderGF;
 import shift.sextiarysector.tileentity.TileEntityBlockBottle;
+import shift.sextiarysector.tileentity.TileEntityFluidCrafter;
 import shift.sextiarysector.tileentity.TileEntityGearShaft;
 import shift.sextiarysector.tileentity.TileEntityMonitor;
 import shift.sextiarysector.tileentity.TileEntitySSChest;
@@ -41,6 +43,8 @@ public class ClientProxy extends CommonProxy{
 
 		this.bottleType = RenderingRegistry.getNextAvailableRenderId();
 
+		this.fluidCrafterType = RenderingRegistry.getNextAvailableRenderId();
+
 		this.ShaftRenderType = RenderingRegistry.getNextAvailableRenderId();
 
 		this.GearShaftRenderType = RenderingRegistry.getNextAvailableRenderId();
@@ -57,6 +61,8 @@ public class ClientProxy extends CommonProxy{
 		RenderingRegistry.registerBlockHandler(new RendererHole());
 
 		RenderingRegistry.registerBlockHandler(new RendererBlockBottle());
+
+		RenderingRegistry.registerBlockHandler(new RendererFluidCrafter());
 
 		RenderingRegistry.registerBlockHandler(new RendererShaft());
 
@@ -81,6 +87,8 @@ public class ClientProxy extends CommonProxy{
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityShaft.class, new RendererShaft());
 
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBlockBottle.class, new RendererBlockBottle());
+
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFluidCrafter.class, new RendererFluidCrafter());
 
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGearShaft.class, new RendererGearShaft());
 
---
+++ src/main/java/shift/sextiarysector/tileentity/TileEntityFluidCrafter.java
@@ -0,0 +1,101 @@
+package shift.sextiarysector.tileentity;
+
+import java.util.Random;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraftforge.fluids.FluidStack;
+
+public class TileEntityFluidCrafter extends TileEntity {
+
+	public static Random r = new Random();
+
+	public FluidStack f ;
+
+	public int x = 0;
+	public int y = 0;
+	public int z = 0;
+
+	@Override
+	public void updateEntity() {
+		super.updateEntity();
+		if (this.worldObj.isRemote) {
+			this.updateClientEntity();
+		} else {
+
+			this.updateServerEntity();
+
+		}
+
+	}
+
+	public void updateClientEntity() {
+
+		x+=r.nextInt(2)+4;
+		y+=r.nextInt(2)+4;
+		z+=r.nextInt(2)+4;
+
+	}
+
+	private void updateServerEntity() {
+
+	}
+
+	public boolean hasFluid() {
+		return f!=null;
+	}
+
+	public IIcon getFluidIcon(){
+    	return this.f.getFluid().getIcon(f);
+    }
+
+    public int getFluidColor(){
+    	return this.f.getFluid().getColor(this.f);
+    }
+
+	@Override
+	public void readFromNBT(NBTTagCompound par1nbtTagCompound) {
+		super.readFromNBT(par1nbtTagCompound);
+		if(par1nbtTagCompound.hasKey("fluid")){
+
+			NBTTagCompound nbt = par1nbtTagCompound.getCompoundTag("fluid");
+
+			this.f = FluidStack.loadFluidStackFromNBT(nbt);
+		}
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound par1nbtTagCompound) {
+		super.writeToNBT(par1nbtTagCompound);
+		if(f!=null){
+
+			NBTTagCompound nbt =  new NBTTagCompound();
+
+			f.writeToNBT(nbt);
+
+			par1nbtTagCompound.setTag("fluid", nbt);
+
+		}
+	}
+
+	@Override
+	public Packet getDescriptionPacket() {
+		NBTTagCompound nbt = new NBTTagCompound();
+		writeToNBT(nbt);
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbt);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+		readFromNBT(pkt.func_148857_g());
+    	//this.worldObj.func_147479_m(xCoord, yCoord, zCoord);
+		this.worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+	}
+
+
+
+}
