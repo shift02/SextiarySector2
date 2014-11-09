9f86c547a000a4807fd2baee2739e89dcd27afac
shift02 <shift02ss@gmail.com>
2014-11-09 20:13:21 +0900

    修正 : レシピのnull
    ・液体カマドのネザーラックレシピを削除
    ・液体を扱うRendererのnullチェックを強化

--- src/main/java/shift/sextiarysector/recipe/RecipesFluidFurnace.java
+++ src/main/java/shift/sextiarysector/recipe/RecipesFluidFurnace.java
@@ -1,6 +1,5 @@
 package shift.sextiarysector.recipe;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
@@ -11,7 +10,7 @@ public class RecipesFluidFurnace {
 	public static void addRecipes(RecipeSimpleFluid recipe)
     {
 
-		recipe.add(new ItemStack(Blocks.netherrack,1),null,  new FluidStack(SSFluids.drinkingWater, 50));
+		//recipe.add(new ItemStack(Blocks.netherrack,1),null,  new FluidStack(SSFluids.drinkingWater, 50));
 		recipe.add(new ItemStack(Items.water_bucket,1),new ItemStack(Items.stick, 1), new FluidStack(SSFluids.drinkingWater, 1000));
 
     }
