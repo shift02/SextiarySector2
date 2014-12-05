4fbb5961d33d899a2d1f64b8810b8cb268dc65e7
shift02 <shift02ss@gmail.com>
2014-12-06 00:16:04 +0900

    追加 : ナイフとクリーパを倒すと頭をドロップ
    ・ナイフに効果を追加
    ・鉱石粉を製錬できるように

--- src/main/java/shift/sextiarysector/SSOreDictionary.java
+++ src/main/java/shift/sextiarysector/SSOreDictionary.java
@@ -9,13 +9,14 @@ public class SSOreDictionary {
 
 		OreDictionary.registerOre("craftingSugar", Items.sugar);
 
-
 		//素材 プレート
 		OreDictionary.registerOre("plateWood", SSBlocks.woodPlate);
 
-
 		OreDictionary.registerOre("gearWood", SSItems.woodGear);
 		OreDictionary.registerOre("gearStone", SSItems.stoneGear);
+
+		OreDictionary.registerOre("dustIron", SSItems.ironDust);
+		OreDictionary.registerOre("dustGold", SSItems.goldDust);
 
 	}
 
