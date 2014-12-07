65ab307c2069b22ef67af2e3fd3a695700f9ef5c
shift02 <shift02ss@gmail.com>
2014-12-07 23:25:49 +0900

    追加 : バージョン表記

--- src/main/java/shift/sextiarysector/SextiarySector.java
+++ src/main/java/shift/sextiarysector/SextiarySector.java
@@ -32,7 +32,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 public class SextiarySector {
 
 	//public static final String MODID = "SextiarySector";
-    public static final String VERSION = "2.0.8";
+    public static final String VERSION = "2.0.9";
 
     @Mod.Instance("SextiarySector")
     public static SextiarySector instance;
