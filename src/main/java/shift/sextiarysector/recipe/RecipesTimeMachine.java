2a6f143fa83ecaf89456c788f0f7b757444bfac4
shift02 <shift02ss@gmail.com>
2016-02-25 22:36:01 +0900

    修正：タイムマシーンのレシピ

--- src/main/java/shift/sextiarysector/recipe/RecipesTimeMachine.java
+++ src/main/java/shift/sextiarysector/recipe/RecipesTimeMachine.java
@@ -2,31 +2,31 @@ package shift.sextiarysector.recipe;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import shift.sextiarysector.SSBlocks;
 import shift.sextiarysector.SSItems;
+import shift.sextiarysector.item.ItemSeed;
 
 public class RecipesTimeMachine {
 
     public static void addRecipes(RecipeSimpleMachine recipe) {
 
-        recipe.add(new ItemStack(SSItems.turnip), new ItemStack(SSBlocks.turnip, 1));
-        recipe.add(new ItemStack(SSItems.cucumber), new ItemStack(SSBlocks.cucumber, 1));
-        recipe.add(new ItemStack(SSItems.ironTurnip), new ItemStack(SSBlocks.ironTurnip, 1));
+        recipe.add(new ItemStack(SSItems.turnip), ItemSeed.getSeedItemStack("turnip", 2));
+        recipe.add(new ItemStack(SSItems.cucumber), ItemSeed.getSeedItemStack("cucumber", 2));
+        recipe.add(new ItemStack(SSItems.ironTurnip), ItemSeed.getSeedItemStack("iron_turnip", 2));
 
-        recipe.add(new ItemStack(SSItems.onion), new ItemStack(SSBlocks.onion, 1));
-        recipe.add(new ItemStack(SSItems.tomato), new ItemStack(SSBlocks.tomato, 1));
-        recipe.add(new ItemStack(SSItems.corn), new ItemStack(SSBlocks.corn, 1));
-        recipe.add(new ItemStack(SSItems.goldenCorn), new ItemStack(SSBlocks.goldenCorn, 1));
+        recipe.add(new ItemStack(SSItems.onion), ItemSeed.getSeedItemStack("onion", 2));
+        recipe.add(new ItemStack(SSItems.tomato), ItemSeed.getSeedItemStack("tomato", 2));
+        recipe.add(new ItemStack(SSItems.corn), ItemSeed.getSeedItemStack("corn", 2));
+        recipe.add(new ItemStack(SSItems.goldenCorn), ItemSeed.getSeedItemStack("golden_corn", 2));
 
-        recipe.add(new ItemStack(SSItems.eggplant), new ItemStack(SSBlocks.eggplant, 1));
-        recipe.add(new ItemStack(SSItems.sweetPotato), new ItemStack(SSBlocks.sweetPotato, 1));
-        recipe.add(new ItemStack(SSItems.greenPepper), new ItemStack(SSBlocks.greenPepper, 1));
+        recipe.add(new ItemStack(SSItems.eggplant), ItemSeed.getSeedItemStack("eggplant", 2));
+        recipe.add(new ItemStack(SSItems.sweetPotato), ItemSeed.getSeedItemStack("sweet_potato", 2));
+        recipe.add(new ItemStack(SSItems.greenPepper), ItemSeed.getSeedItemStack("green_pepper", 2));
 
-        recipe.add(new ItemStack(SSItems.radish), new ItemStack(SSBlocks.radish, 1));
+        recipe.add(new ItemStack(SSItems.radish), ItemSeed.getSeedItemStack("radish", 2));
 
-        recipe.add(new ItemStack(SSItems.rice), new ItemStack(SSBlocks.rice, 1));
+        recipe.add(new ItemStack(SSItems.rice), ItemSeed.getSeedItemStack("rice", 2));
 
-        recipe.add(new ItemStack(Blocks.red_mushroom), new ItemStack(SSBlocks.redMushroom, 1));
+        recipe.add(new ItemStack(Blocks.red_mushroom), ItemSeed.getSeedItemStack("mushroom_red", 2));
 
         recipe.add(new ItemStack(SSItems.attackRustUnit), new ItemStack(SSItems.attackUnit, 1));
         recipe.add(new ItemStack(SSItems.defenseRustUnit), new ItemStack(SSItems.defenseUnit, 1));
