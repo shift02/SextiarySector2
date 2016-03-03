4a7a6cf71a1f153baf38819bbd5096c1f3115cd8
shift02 <shift02ss@gmail.com>
2016-03-03 23:27:26 +0900

    修正：Shop機能

--- src/main/java/shift/sextiarysector/api/shop/IShopMemory.java
+++ src/main/java/shift/sextiarysector/api/shop/IShopMemory.java
@@ -1,12 +1,19 @@
 package shift.sextiarysector.api.shop;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
+/**
+ * 新しいShopを追加するためのインターフェース<br>
+ * Itemに実装
+ * @author Shift02
+ *
+ */
 public interface IShopMemory {
 
     public ResourceLocation getMonitorResource();
 
-    public int getShopID(World world);
+    public int getShopID(World world, EntityPlayer player);
 
 }
