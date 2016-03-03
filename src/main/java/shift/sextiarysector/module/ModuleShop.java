422d6842063bbdcbc87fb39c24d4530e04c289ea
shift02 <shift02ss@gmail.com>
2016-03-03 23:34:56 +0900

    修正：商品の被り

--- src/main/java/shift/sextiarysector/module/ModuleShop.java
+++ src/main/java/shift/sextiarysector/module/ModuleShop.java
@@ -46,7 +46,6 @@ public class ModuleShop implements IModule {
         creeper = new ShopSeasonBase("shop.ss.creeper");
 
         creeper.addProduct(new ProductBase(new ItemStack(SSItems.blueStoneDust, 2), 320));
-        creeper.addProduct(new ProductBase(new ItemStack(SSItems.blueStoneDust, 2), 320));
         creeper.addProduct(new ProductBase(new ItemStack(SSItems.yellowStoneDust, 2), 320));
 
         creeper.addProduct(new ProductBase(new ItemStack(SSItems.takumiTeaBottle, 2), 216));
