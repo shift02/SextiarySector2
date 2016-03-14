5a7eefe496c6bdfe92b1d10a52d9aabadb10b441
shift02 <shift02ss@gmail.com>
2016-03-14 15:15:38 +0900

    追加：金床レシピ

---
+++ src/main/java/shift/sextiarysector/event/AnvilEventHandler.java
@@ -0,0 +1,47 @@
+/*
+* 作成者: Shift02
+* 作成日: 2016/03/11 - 16:40:31
+*/
+package shift.sextiarysector.event;
+
+public class AnvilEventHandler {
+
+    //    @SubscribeEvent
+    //    public void onSlotUpdateEvent(AnvilUpdateEvent event) {
+    //
+    //        ItemStack leftSlot = event.left;
+    //        ItemStack rightSlot = event.right;
+    //        ItemStack output = event.output;
+    //
+    //        if (leftSlot == null && rightSlot == null) return;
+    //
+    //        if (output != null) return;
+    //
+    //        output = null;//SSRecipes.anvil.getResult(leftSlot, rightSlot);
+    //
+    //        if (output == null) return;
+    //
+    //        event.output = output;
+    //        event.cost = 1;
+    //        event.materialCost = 1;
+    //        //event.name = output.getDisplayName();
+    //
+    //    }
+    //
+    //    @SubscribeEvent
+    //    public void onRepairEvent(AnvilRepairEvent event) {
+    //
+    //        ItemStack leftSlot = event.output;
+    //        ItemStack rightSlot = event.left;
+    //
+    //        if (leftSlot == null && rightSlot == null) return;
+    //        System.out.println(leftSlot + "" + rightSlot);
+    //
+    //        if (SSRecipes.anvil.getResult(leftSlot, rightSlot) != null) {
+    //            event.breakChance = 0;
+    //            leftSlot.stackSize++;
+    //        }
+    //
+    //    }
+
+}
---
+++ src/main/java/shift/sextiarysector/recipe/RecipesAnvil.java
@@ -0,0 +1,15 @@
+/*
+* 作成者: Shift02
+* 作成日: 2016/03/11 - 16:15:59
+*/
+package shift.sextiarysector.recipe;
+
+public class RecipesAnvil {
+
+    public static void addRecipes(RecipeAnvil recipe) {
+
+        //recipe.add("ingotIron", "ingotIron", true, new ItemStack(SSBlocks.ironPlate));
+
+    }
+
+}
