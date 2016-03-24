1acb6b0fec97557a3c3ad238431d2262f5a53658
shift02 <shift02ss@gmail.com>
2016-03-24 17:24:38 +0900

    追加：プレートのレシピ

--- src/main/java/shift/sextiarysector/recipe/RecipesVanilla.java
+++ src/main/java/shift/sextiarysector/recipe/RecipesVanilla.java
@@ -1,9 +1,11 @@
 package shift.sextiarysector.recipe;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class RecipesVanilla {
 
@@ -33,6 +35,17 @@ public class RecipesVanilla {
                         Character.valueOf('x'), "stickWood",
                         Character.valueOf('y'), new ItemStack(Items.string),
                         Character.valueOf('z'), "nuggetZinc"
+                }));
+
+        //金
+        p_77608_1_.getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Blocks.light_weighted_pressure_plate, 1),
+                new Object[] {
+                        "plateGold"
+                }));
+
+        p_77608_1_.getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Blocks.heavy_weighted_pressure_plate, 1),
+                new Object[] {
+                        "plateIron"
                 }));
 
     }
