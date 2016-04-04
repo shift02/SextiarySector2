49284501d4eeda8daf3cab4cb3f8ea3c941d2420
shift02 <shift02ss@gmail.com>
2016-04-04 16:25:29 +0900

    変更：バージョン

--- src/main/java/shift/sextiarysector/SextiarySector.java
+++ src/main/java/shift/sextiarysector/SextiarySector.java
@@ -38,7 +38,7 @@ import shift.sextiarysector.recipe.RecipesFurnaceCraft;
 public class SextiarySector {
 
     public static final String MODID = "SextiarySector";
-    public static final String VERSION = "2.4.7";
+    public static final String VERSION = "2.4.8";
 
     @Mod.Instance("SextiarySector")
     public static SextiarySector instance;
