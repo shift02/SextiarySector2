c11e328193a4eb5f2562b32a67d0852d83cc963a
shift02 <shift02ss@gmail.com>
2016-04-16 22:24:00 +0900

    修正：バニラかまどにnullがあるときの問題

--- src/main/java/shift/sextiarysector/recipe/RecipesFurnaceCraft.java
+++ src/main/java/shift/sextiarysector/recipe/RecipesFurnaceCraft.java
@@ -306,12 +306,18 @@ public class RecipesFurnaceCraft {
 
         for (Map.Entry<ItemStack, ItemStack> e : ((HashMap<ItemStack, ItemStack>) FurnaceRecipes.smelting().getSmeltingList()).entrySet()) {
 
-            if (e.getValue().getItem().hasContainerItem(e.getValue().copy()) && e.getKey().getItem().hasContainerItem(e.getKey().copy())) {
-                if (checkItem(e.getValue().getItem().getContainerItem(e.getValue().copy()), e.getKey().getItem().getContainerItem(e.getKey().copy())))
-                    continue;
-            }
+            try {
 
-            FurnaceCraftingManager.getInstance().addShapelessRecipe(e.getValue(), new Object[] { e.getKey() });
+                if (e.getValue().getItem().hasContainerItem(e.getValue().copy()) && e.getKey().getItem().hasContainerItem(e.getKey().copy())) {
+                    if (checkItem(e.getValue().getItem().getContainerItem(e.getValue().copy()), e.getKey().getItem().getContainerItem(e.getKey().copy())))
+                        continue;
+                }
+
+                FurnaceCraftingManager.getInstance().addShapelessRecipe(e.getValue(), new Object[] { e.getKey() });
+
+            } catch (NullPointerException nullE) {
+
+            }
 
         }
 
